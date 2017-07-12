e227b73c7db26e857e359dadc63ec6b5231ac256
ZC <zengcheng263@gmail.com>
2017-07-13 01:07:53 +0800

    fill the blank. set the button to logout.

--- index.php
+++ index.php
@@ -15,9 +15,14 @@
 </head>
 <body>
 	<div id="div_allcontent">
-		<header id="header">
-			<span><a href="/login/logout.php">exit</a></span>
-		</header>
+		<div id="div_header">
+			<header id="header"><!--设置ul会把header撑大-->
+				<ul id="ul_header">
+					<li id="header_li_exit"><a class="a_in_ul_header_li" href="/login/logout.php">exit</a></li>
+					<li id="header_li_set"><a class="a_in_ul_header_li" href="/login/logout.php">set</a></li>
+				</ul>
+			</header>
+		</div>
 		<nav>
 			<ul id="ul_nav">
 				<li class="li_ul_nav"><a href="https://www.baidu.com">baidu</a></li>
@@ -25,21 +30,21 @@
 				<li class="li_ul_nav"><a href="https://www.taobao.com">taobao</a></li>
 			</ul>
 		</nav>
+		<div id="div_main_three">
+			<aside>
+				<div class="div_aside_block"><a href="lipage1.html" target="mainiframe">dota2</a></div>
+				<div class="div_aside_block"><a href="lipage2.html" target="mainiframe">github</a></div>
+				<div class="div_aside_block"><a href="lipage3.html" target="mainiframe">stackoverflow</a></div>
+			</aside>
 
-		<aside>
-			<div class="div_aside_block"><a href="lipage1.html" target="mainiframe">dota2</a></div>
-			<div class="div_aside_block"><a href="lipage2.html" target="mainiframe">github</a></div>
-			<div class="div_aside_block"><a href="lipage3.html" target="mainiframe">stackoverflow</a></div>
-		</aside>
-
-		<div id="div_iframe">
-		<iframe name="mainiframe" src="lipage1.html">
-		</iframe>
+			<div id="div_iframe">
+				<iframe name="mainiframe" src="lipage1.html"></iframe>
+			</div>
 		</div>
-
 		<footer>
 			<img src="/img/index/header_bkg.png"/>
 		</footer>
+
 	</div>
 
 	<script type="text/javascript" scr="index.js"></script>
